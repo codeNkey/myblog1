e15dd80b0d71181a994584b495d76aee0d81b8af
heejin2dat@naver.com <heejin2dat@naver.com>
2024-11-07 17:24:00 +0900

    장고 마지막

---
+++ api/views.py
@@ -0,0 +1,84 @@
+from django.http import JsonResponse
+from django.views.generic.list import BaseListView
+from django.views.generic.detail import BaseDetailView
+from django.views import View
+from django.views.generic.edit import BaseCreateView
+from api.utils import obj_to_post, prev_next_post, obj_to_comment
+from blog.models import Post, Category, Tag, Comment
+
+# Create your views here.
+
+class ApiPostLV(BaseListView):
+
+    def get_queryset(self):
+        paramCate = self.request.GET.get('category')
+        paramTag = self.request.GET.get('tag')
+        print('-----------')
+        print('-----------')
+        print('-----------')
+        print(paramCate, paramTag)
+        if paramCate:
+            qs = Post.objects.filter(category__name__iexact= paramCate)
+        elif paramTag:
+            qs = Post.objects.filter(tags__name__iexact= paramTag)
+        else:
+            qs = Post.objects.all()
+        return qs
+
+    def render_to_response(self, context, **response_kwargs):
+        qs = context['object_list']
+        postList = [ obj_to_post(obj, False) for obj in qs]
+        return JsonResponse(data=postList, safe=False, status=200)
+    
+class ApiPostDV(BaseDetailView):
+    model = Post
+
+    def render_to_response(self, context, **response_kwargs):
+        obj = context['object']
+        post = obj_to_post(obj)
+        prevPost, nextPost = prev_next_post(obj)
+
+        commentqs = obj.comment_set.all()
+        commentList = [obj_to_comment(obj) for obj in commentqs]
+
+        jsonData = {
+            'post': post,
+            'prevPost': prevPost,
+            'nextPost': nextPost,
+            'commentList': commentList,
+        }
+
+        return JsonResponse(data=jsonData, safe=True, status=200)
+    
+class ApiCateTagView(View):
+    def get(self, request,*args, **kwargs):
+        qs1 = Category.objects.all()
+        qs2 = Tag.objects.all()
+        cateList = [cate.name for cate in qs1]
+        tagList = [tag.name for tag in qs2]
+        jsonData = {
+            'cateList': cateList,
+            'tagList': tagList
+        }
+        return JsonResponse(data=jsonData, safe=True, status=200)
+    
+class ApiPostLikeDV(BaseDetailView):
+    model = Post
+
+    def render_to_response(self, context, **response_kwargs):
+        obj = context['object']
+        obj.like += 1
+        obj.save()
+        return JsonResponse(data={'like': obj.like}, safe=False, status=200)
+
+class ApiCommentCV(BaseCreateView):
+    model = Comment 
+    fields = '__all__'
+
+    def form_valid(self, form):
+        self.object = form.save()
+        comment = obj_to_comment(self.object)
+        return JsonResponse(data=comment, safe=True, status=200)
+
+    def form_invalid(self, form):
+        return JsonResponse(data=form.errors, safe=True, status=400)
---
+++ blog/models.py
@@ -0,0 +1,55 @@
+from django.db import models
+
+# Create your models here.
+
+# 게시글
+class Post(models.Model):
+    # FK(Foreign Key)
+    category = models.ForeignKey('Category', blank=True, null=True, on_delete=models.SET_NULL)
+    tags = models.ManyToManyField('Tag', blank=True)
+    
+    title = models.CharField('TITLE', max_length=50)
+    description = models.CharField('DESCRIPTION', max_length=100, 
+                                   blank=True, 
+                                   help_text='simple one-line text.')
+    image = models.ImageField('IMAGE', upload_to='blog/%Y/%m/',
+                              blank=True, null= True)
+    content = models.TextField('CONTENT')
+    create_date = models.DateTimeField("CREATE DT", auto_now_add=True)
+    update_date = models.DateTimeField("UPDATE DT", auto_now=True)
+    like = models.PositiveIntegerField('LIKE', default=0)
+
+    def __str__(self):
+        return self.title
+
+## 게시글의 종류를 지정
+class Category(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    description = models.CharField('DESCRIPTION', max_length=100,
+                                   blank=True,
+                                   help_text='simple one-line text.')
+
+    def __str__(self):
+        return self.name
+
+
+## 게시글의 해시 태그
+class Tag(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+## 댓글
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    content = models.TextField('CONTENT')
+    create_date = models.DateTimeField('CREATE DT', auto_now_add=True)
+    update_date = models.DateTimeField('UPDATE DT', auto_now=True)
+
+    @property
+    def short_content(self):
+        return self.content[:10]
+    
+    def __str__(self):
+        return self.short_content
